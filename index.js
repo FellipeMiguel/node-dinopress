e0f9b8b33250ef3ebe050bb5643116a86c087dd7
FellipeMiguel <fellipemiguel1@icloud.com>
2023-10-10 16:19:02 -0300

    feat: Articles and Categories controllers created

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const bodyParser = require('body-parser');
 const connection = require('./database/database');
 
+// Rotas
+const categoriesController = require('./categories/CategoriesController');
+const articlesController = require('./articles/ArticlesController');
+
 // View engine
 app.set('view engine', 'ejs');
 
@@ -19,6 +23,10 @@ connection.authenticate().then(() => {
 }).catch(err => {
     console.log(err)
 });
+
+// Rotas
+app.use('/', categoriesController);
+app.use('/', articlesController);
 
 app.get("/", (req, res) => {
     res.render('index');
