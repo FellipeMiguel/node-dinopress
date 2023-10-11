77405a555d06f70d28d23fb215161162765bbbfd
FellipeMiguel <fellipemiguel1@icloud.com>
2023-10-11 16:03:20 -0300

    feat: Article saved in database

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const Category = require("../categories/Category");
 const router = express.Router();
+const Article = require('./Article');
+const slugify = require('slugify');
 
-router.get('/articles', (req, res) => {
+router.get('/admin/articles', (req, res) => {
     res.send('rotas de artigo');
 });
 
 router.get('/admin/articles/new', (req, res) => {
-    res.render('admin/articles/new');
+    Category.findAll().then(categories => {
+        res.render('admin/articles/new', {categories: categories});
+    });
 });
+
+// Salvando artigo no banco de dados
+router.post('/articles/save', (req, res) => {
+    const {title, body, category} = req.body;
+
+    Article.create({
+        title: title,
+        slug: slugify(title).toLowerCase(),
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect('/admin/articles')
+    })
+})
 
 module.exports = router;
