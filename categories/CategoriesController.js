2fca31be6a02c563f205f22c692717fbc6aa521c
FellipeMiguel <fellipemiguel1@icloud.com>
2023-10-11 01:19:31 -0300

    feat: deleting categories works

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -19,12 +19,31 @@ router.post('/categories/save', (req, res) => {
     } else {
         res.redirect('/admin/categories/new');
     }
-})
+});
 
 router.get('/admin/categories', (req, res) => {
     Categoty.findAll().then(categories => {
         res.render('admin/categories/index', {categories: categories});
-    })
-})
+    });
+});
+
+router.post('/categories/delete', (req, res) => {
+    const id = req.body.id;
+    if (id != undefined) {
+        if (!isNaN(id)) {
+            Categoty.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect('/admin/categories');
+            });
+        } else {
+            res.redirect('/admin/categories');
+        }
+    } else {
+        res.redirect('/admin/categories');
+    }
+});
 
 module.exports = router;
