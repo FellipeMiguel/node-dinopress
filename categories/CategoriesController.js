e2965ce217543489b3307e8f390489522e4da924
FellipeMiguel <fellipemiguel1@icloud.com>
2023-10-11 15:07:01 -0300

    feat: update category created

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -14,7 +14,7 @@ router.post('/categories/save', (req, res) => {
             title: title,
             slug: slugify(title).toLowerCase()
         }).then(() => {
-            res.redirect('/');
+            res.redirect('/admin/categories');
         })
     } else {
         res.redirect('/admin/categories/new');
@@ -44,6 +44,37 @@ router.post('/categories/delete', (req, res) => {
     } else {
         res.redirect('/admin/categories');
     }
+});
+
+router.get('/admin/categories/edit/:id', (req, res) => {
+    const id = req.params.id;
+
+    if(isNaN(id)) {
+        res.redirect('/admin/categories');
+    }
+
+   Categoty.findByPk(id).then(category => {
+       if(category != undefined) {
+            res.render('admin/categories/edit', {category: category})
+       }else {
+           res.redirect('/admin/categories');
+       }
+   }).catch(err => {
+       res.redirect('/admin/categories');
+   });
+});
+
+router.post('/categories/update', (req, res) => {
+   const id = req.body.id;
+   const title = req.body.title;
+
+   Categoty.update({title: title, slug: slugify(title).toLowerCase()}, {
+       where: {
+           id: id
+       }
+   }).then(() => {
+       res.redirect('/admin/categories');
+   });
 });
 
 module.exports = router;
