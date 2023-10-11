f2a1e71ff363a44b7f230316db49c4971e3f216c
FellipeMiguel <fellipemiguel1@icloud.com>
2023-10-11 16:21:27 -0300

    feat: Article's page

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -3,9 +3,14 @@ const Category = require("../categories/Category");
 const router = express.Router();
 const Article = require('./Article');
 const slugify = require('slugify');
+const Categoty = require("../categories/Category");
 
 router.get('/admin/articles', (req, res) => {
-    res.send('rotas de artigo');
+    Article.findAll({
+        include: [{model: Category}]
+    }).then(articles => {
+        res.render('admin/articles/index', {articles: articles});
+    });
 });
 
 router.get('/admin/articles/new', (req, res) => {
@@ -25,7 +30,26 @@ router.post('/articles/save', (req, res) => {
         categoryId: category
     }).then(() => {
         res.redirect('/admin/articles')
-    })
-})
+    });
+});
+
+router.post('/articles/delete', (req, res) => {
+    const id = req.body.id;
+    if (id != undefined) {
+        if (!isNaN(id)) {
+            Article.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect('/admin/articles');
+            });
+        } else {
+            res.redirect('/admin/articles');
+        }
+    } else {
+        res.redirect('/admin/articles');
+    }
+});
 
 module.exports = router;
