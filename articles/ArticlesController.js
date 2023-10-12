bf2a13954ae256111fc7f72367d2c6b125fcdd97
FellipeMiguel <fellipemiguel1@icloud.com>
2023-10-11 22:47:40 -0300

    feat: Articles are updatable now

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -52,4 +52,33 @@ router.post('/articles/delete', (req, res) => {
     }
 });
 
+router.get('/admin/articles/edit/:id', (req, res) => {
+    const {id} = req.params;
+    Article.findByPk(id).then(article => {
+        if (article != undefined) {
+            Categoty.findAll().then(categories => {
+                res.render('admin/articles/edit', {categories: categories, article: article});
+            });
+        } else {
+            res.redirect('/');
+        }
+    }).catch(err => {
+        res.redirect('/');
+    });
+});
+
+router.post('/articles/update', (req, res) => {
+   const {id, title, body, category} = req.body;
+
+   Article.update({title: title, body: body, categoryId: category, slug: slugify(title).toLowerCase()},{
+       where: {
+           id: id
+       }
+   }).then(() => {
+       res.redirect('/admin/articles');
+   }).catch(err => {
+       res.redirect('/');
+   });
+});
+
 module.exports = router;
